af8b9b67b41300edec5e0af9b435e5c22381c126
EVAN VOORDECKERS <u1087559@umail.utah.edu>
2019-08-25 21:55:00 -0600

    Program passes both examples given in the assignment, and is using the algorithm discussed in class

--- Assignment PS1-4/Assignment PS1-4/Program.cs
+++ Assignment PS1-4/Assignment PS1-4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Assignment_PS1_4
 {
@@ -6,8 +8,46 @@ namespace Assignment_PS1_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            // test comment
+            HashSet<string> dictionary = new HashSet<string>();
+            HashSet<string> approved = new HashSet<string>();
+            HashSet<string> rejected = new HashSet<string>();
+
+            // Read the first line and parse the ints
+            string firstLine = Console.ReadLine();
+            List<int> firstLineNums = new List<int>();
+            foreach (string num in firstLine.Split(new char[] { ' ' }))
+            {
+                if (Int32.TryParse(num, out int result))
+                {
+                    firstLineNums.Add(result);
+                }
+            }
+
+            // Read through the rest of the lines to create our "dictionary"
+            string line;
+            for(int i = 0; i < firstLineNums[0]; i++)
+            {
+                line = Console.ReadLine();
+                dictionary.Add(line);
+            }
+
+            // count/print anagrams
+            foreach (string word in dictionary)
+            {
+                string sortedWord = String.Concat(word.OrderBy(c => c));
+
+                if (approved.Contains(sortedWord))
+                {
+                    approved.Remove(sortedWord);
+                    rejected.Add(sortedWord);
+                }
+                else if (!rejected.Contains(sortedWord))
+                {
+                    approved.Add(sortedWord);
+                }
+            }
+
+            Console.WriteLine(approved.Count);
         }
     }
 }
