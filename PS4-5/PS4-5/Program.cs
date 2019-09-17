30114838dbc626d2c2630f721c399eb23d35418c
ChaoticEvan <evan.voordeckers@yahoo.com>
2019-09-17 13:20:40 -0600

    Built out basic structure for PS4-5

---
+++ PS4-5/PS4-5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PS4_5
+{
+    class Program
+    {
+        Dictionary<string, Vertex> graph;
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Vertex
+    {
+        HashSet<Edge> leavingEdges { get; set; }
+        HashSet<Edge> incomingEdges { get; set; }
+
+        string name;
+
+        public Vertex(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    class Edge
+    {
+        int weight;
+        Vertex from;
+        Vertex to;
+
+        Edge(int weight, Vertex from, Vertex to)
+        {
+            this.weight = weight;
+            this.from = from;
+            this.to = to;
+        }
+    }
+}
