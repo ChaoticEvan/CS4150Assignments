08f7ea963ec725e4962a379af55f29158ff87ef7
ChaoticEvan <evan.voordeckers@yahoo.com>
2019-09-17 13:58:23 -0600

    Graph is building out

--- PS4-5/PS4-5/Program.cs
+++ PS4-5/PS4-5/Program.cs
@@ -5,37 +5,67 @@ namespace PS4_5
 {
     class Program
     {
-        Dictionary<string, Vertex> graph;
+
+        static Dictionary<string, Vertex> graph;
         static void Main(string[] args)
         {
-            
+            string currLine = "";
+            graph = new Dictionary<string, Vertex>();
+
+            // Build out the vertexes given in the first
+            // group of info
+            currLine = Console.ReadLine();
+            Int32.TryParse(currLine, out int numVertexes);
+            for (int i = 0; i < numVertexes; i++)
+            {
+                currLine = Console.ReadLine();
+                string[] currLineTokens = currLine.Split(' ');
+
+                graph.Add(currLineTokens[0], new Vertex(currLineTokens[0], Int32.Parse(currLineTokens[1])));
+            }
+
+            // Build out the edges given in the second group 
+            // of info
+            currLine = Console.ReadLine();
+            Int32.TryParse(currLine, out int numEdges);
+            for (int j = 0; j < numEdges; j++)
+            {
+                currLine = Console.ReadLine();
+                string[] currLineTokens = currLine.Split(' ');
+
+                graph[currLineTokens[0]].leavingEdges.Add(currLineTokens[1]);
+                graph[currLineTokens[1]].incomingEdges.Add(currLineTokens[0]);
+            }
+
+            // 
+            currLine = Console.ReadLine();
+            Int32.TryParse(currLine, out int numTrips);
+            for(int k = 0; k < numTrips; k++)
+            {
+                currLine = Console.ReadLine();
+                string[] currLineTokens = currLine.Split(' ');
+
+                string from = currLineTokens[0];
+                string to = currLineTokens[1];
+
+                // TODO DO DJIKSTRA'S to find cheapest path from from to to
+            }
         }
     }
 
     class Vertex
     {
-        HashSet<Edge> leavingEdges { get; set; }
-        HashSet<Edge> incomingEdges { get; set; }
+        public HashSet<string> leavingEdges { get; set; }
+        public HashSet<string> incomingEdges { get; set; }
+        public int cost { get; set; }
+        public string name { get; set; }
 
-        string name;
-
-        public Vertex(string name)
+        public Vertex(string name, int cost)
         {
+            leavingEdges = new HashSet<string>();
+            incomingEdges = new HashSet<string>();
+            this.cost = cost;
             this.name = name;
-        }
-    }
-
-    class Edge
-    {
-        int weight;
-        Vertex from;
-        Vertex to;
-
-        Edge(int weight, Vertex from, Vertex to)
-        {
-            this.weight = weight;
-            this.from = from;
-            this.to = to;
         }
     }
 }
