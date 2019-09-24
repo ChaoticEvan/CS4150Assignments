f56ffae5e1657e9618514a55072a8af089b1c311
ChaoticEvan <evan.voordeckers@yahoo.com>
2019-09-24 15:55:14 -0600

    INIT Commit. Graph is building correctly

---
+++ PS5-6/PS5-6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PS5_6
+{
+    class Program
+    {
+        static Dictionary<string, Student> graph;
+        static void Main(string[] args)
+        {
+            string currLine = "";
+            graph = new Dictionary<string, Student>();
+
+            // Add every student to our graph
+            currLine = Console.ReadLine();
+            Int32.TryParse(currLine, out int numStudents);
+            for (int i = 0; i < numStudents; i++)
+            {
+                currLine = Console.ReadLine();
+                string[] currLineTokens = currLine.Split(' ');
+
+                graph.Add(currLineTokens[0], new Student(currLineTokens[0]));
+            }
+
+            // Build out the edges given in the second group 
+            // of info
+            currLine = Console.ReadLine();
+            Int32.TryParse(currLine, out int numFriendPairs);
+            for (int j = 0; j < numFriendPairs; j++)
+            {
+                currLine = Console.ReadLine();
+                string[] currLineTokens = currLine.Split(' ');
+                graph[currLineTokens[0]].friends.Add(currLineTokens[1]);
+                graph[currLineTokens[1]].friends.Add(currLineTokens[0]);
+            }
+
+            // debugging line
+        }
+    }
+
+    class Student
+    {
+        public HashSet<string> friends { get; set; }
+        public string name { get; set; }
+        public bool visited { get; set; }
+
+        public Student(string name)
+        {
+            friends = new HashSet<string>();
+            this.name = name;
+        }
+    }
+}
