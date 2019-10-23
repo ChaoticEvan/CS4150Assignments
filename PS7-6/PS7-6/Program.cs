1886b3c5d5e79f34082c64efe631728ac05e2205
ChaoticEvan <evan.voordeckers@yahoo.com>
2019-10-23 13:43:23 -0600

    Test primality is still not working. Might be worth checking other psuedo code examples

--- PS7-6/PS7-6/Program.cs
+++ PS7-6/PS7-6/Program.cs
@@ -9,6 +9,8 @@ namespace PS7_6
         {
             string currLine = "";
             HashSet<string> results = new HashSet<string>();
+
+            // Loop through input and perform arithmetic based on given case for each line
             while (!String.IsNullOrEmpty(currLine = Console.ReadLine()))
             {
                 string[] currLineTokens = currLine.Split(" ");
@@ -21,17 +23,30 @@ namespace PS7_6
                     case "exp":
                         results.Add(exp(Int32.Parse(currLineTokens[1]), Int32.Parse(currLineTokens[2]), Int32.Parse(currLineTokens[3])).ToString());
                         break;
+
+                    case "inverse":
+                        results.Add(inverse(Int32.Parse(currLineTokens[1]), Int32.Parse(currLineTokens[2])));
+                        break;
+
+                    case "isprime":
+                        results.Add(isPrime(Int32.Parse(currLineTokens[1])));
+                        break;
                 }
-
-
             }
 
-            foreach(string s in results)
+            // Print out results
+            foreach (string s in results)
             {
                 Console.WriteLine(s);
             }
         }
 
+        /// <summary>
+        /// Algorithm for computing GCD from Slides
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>Python equivalent of Triplet</returns>
         private static GcdContainer gcd(int a, int b)
         {
             GcdContainer result = new GcdContainer();
@@ -44,11 +59,21 @@ namespace PS7_6
             }
             else
             {
-                result = gcd(b, a % b);
+                GcdContainer temp = gcd(b, a % b);
+                result.x = temp.y;
+                result.y = temp.x - (a / b) * temp.y;
+                result.d = temp.d;
                 return result;
             }
         }
 
+        /// <summary>
+        /// Algorithm for computing exp from Slides
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="N"></param>
+        /// <returns>Answer</returns>
         private static int exp(int x, int y, int N)
         {
             if (y == 0)
@@ -69,6 +94,61 @@ namespace PS7_6
             }
         }
 
+        /// <summary>
+        /// Algorithm for computing inverse from Slides
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="N"></param>
+        /// <returns>Returns inverse if it exists, or returns "none"</returns>
+        private static string inverse(int a, int N)
+        {
+            GcdContainer cont = gcd(a, N);
+            if (cont.d == 1)
+            {
+                return (cont.x % N).ToString();
+            }
+            else
+            {
+                return "none";
+            }
+        }
+
+        /// <summary>
+        /// Algorithm for finding if N is Prime from slides
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns>Returns string yes or no corresponding to if N is prime</returns>
+        private static string isPrime(int N)
+        {
+            bool result = testPrimality(N);
+            if (result)
+            {
+                return "yes";
+            }
+            else
+            {
+                return "no";
+            }
+        }
+
+        private static bool testPrimality(int N)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int currTest = rand.Next(N - 1) + 1;
+                int pow = (int)Math.Pow(currTest, N - 1);
+                if (pow % N != 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Struct for replicating Python's Triplet
+        /// </summary>
         private struct GcdContainer
         {
             public int x { get; set; }
