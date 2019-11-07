af7427d77da9a2daa1d4c12080d72948fcb6a306
ChaoticEvan <evan.voordeckers@yahoo.com>
2019-11-07 13:21:01 -0700

    Initital commit for PS9-5

---
+++ PS9-5/PS9-5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PS9_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32.TryParse(Console.ReadLine(), out int numOfLines);
+            
+            // Using numOfLines + 1 and Less then or equal to num of lines
+            // Because for some reason the number of lines is + 1 of the given number
+            int[] distance = new int[numOfLines + 1];
+            for(int i = 0; i <= numOfLines; ++i)
+            {
+                Int32.TryParse(Console.ReadLine(), out int temp);
+                distance[i] = temp;
+            }
+            return;
+        }
+    }
+}
