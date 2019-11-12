b6d029b4fe9820d53d2f20dd9f651e510e0299ff
ChaoticEvan <evan.voordeckers@yahoo.com>
2019-11-12 15:58:24 -0700

    Init commit

---
+++ PS10-4/PS10-4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PS10_4
+{
+    class Program
+    {
+        private static Node[,] gallery;
+        static void Main(string[] args)
+        {
+            string currLine = Console.ReadLine();
+            string[] currLineTokens = currLine.Split(" ");
+
+            Int32.TryParse(currLineTokens[0], out int numOfRows);
+
+
+            gallery = new Node[numOfRows, 2];
+
+            for (int i = 0; i < numOfRows; ++i)
+            {
+                // Parse the current line and add the nodes to our 2D array
+                currLine = Console.ReadLine();
+                currLineTokens = currLine.Split(" ");
+                gallery[i, 0] = new Node()
+                {
+                    value = Int32.Parse(currLineTokens[0]),
+                    canClose = true
+                };
+                gallery[i, 1] = new Node()
+                {
+                    value = Int32.Parse(currLineTokens[1]),
+                    canClose = true
+                };
+            }
+        }
+    }
+
+    public struct Node
+    {
+        public int value { get; set; }
+        public bool canClose { get; set; }
+    }
+}
